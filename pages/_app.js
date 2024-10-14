54067972373bf66406df2d7e5a495c0c035c2902
Jeff Yau <jyau11@myseneca.ca>
2024-10-14 01:16:56 -0400

    added port constant

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,19 @@
-import '@/styles/globals.css'
-import Error from 'next/error'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Layout from '@/src/Component/layout'
-import { SWRConfig } from 'swr'
+import "@/styles/globals.css";
+import Error from "next/error";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from "@/src/Component/layout";
+import { SWRConfig } from "swr";
+const port = process.env.PORT || 10000;
 
 const fetcher = async (...args) => {
-  const response = await fetch(...args)
+  const response = await fetch(...args);
 
   if (!response.ok) {
-    throw new Error(`Request failed with status: ${response.status}`)
+    throw new Error(`Request failed with status: ${response.status}`);
   }
 
-  return response.json()
-}
+  return response.json();
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -23,5 +24,5 @@ export default function App({ Component, pageProps }) {
         </Layout>
       </SWRConfig>
     </>
-  )
+  );
 }
