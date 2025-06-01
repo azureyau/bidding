2b96d37cd922e4abcf5a7fc25fac0b840b481b71
Jeff Yau <jyau11@myseneca.ca>
2025-06-01 13:23:00 -0400

    home page updated

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
 export default function Home() {
   return (
-    <>
-      <p>this is Home</p>
-    </>
+    <div className='container mt-4'>
+      <h2>Release Notes</h2>
+
+      <h4 className='mt-4'>June 1, 2025</h4>
+      <ul>
+        <li>
+          New feature: Restricted access to certain pages (only accessible by
+          approved user after logged in)
+        </li>
+        <li>
+          New feature: User stays logged in after app refresh / closed and
+          re-open
+        </li>
+      </ul>
+
+      <h4 className='mt-4'>Nov 16, 2024</h4>
+      <ul>
+        <li>New feature: Log in feature</li>
+        <li>
+          Revised feature: Add bidding button available only after logged in
+        </li>
+        <li>
+          Revised feature: Editing bidding button available only after logged in
+        </li>
+      </ul>
+
+      <h4 className='mt-4'>Nov 15, 2024</h4>
+      <ul>
+        <li>New feature: Add bidding button</li>
+        <li>New feature: Revise bidding button</li>
+        <li>Object structure of bidding revised</li>
+      </ul>
+    </div>
   )
 }
